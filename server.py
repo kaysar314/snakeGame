bb97ab2f879920a3b024449da7e03f8f3a2adf65
海萨尔·阿斯哈尔 <hse0724@175game.com>
2016-10-19 18:16:30 +0800

    update

--- server.py
+++ server.py
@@ -2,21 +2,29 @@
 import SocketServer 
 from time import ctime 
 import simplejson
+import threading
+
+lock = threading.Lock()
 
 player = {}
 foods = {}
 data = {}
 
-HOST = '139.129.59.141' 
-PORT = 9080 
+HOST = '127.0.0.1' 
+PORT = 8080 
 ADDR = (HOST, PORT) 
 
 class MyRequestHandler(SocketServer.BaseRequestHandler): 
    def handle(self): 
-       print '...connected from:', self.client_address
+       # print '...connected from:', self.client_address
        while True: 
-            dic = simplejson.loads(self.request.recv(10240))
-
+            lock.acquire()
+            print self.client_address
+            try:
+                ddd = self.request.recv(10240)
+            except:
+                self.close()
+            dic = simplejson.loads(ddd)
             data[dic["name"]] = {"snakePos":dic["mySnakePos"],"color":dic["color"],"live":dic["live"]}
             sendData = {}
             sendData["map"] = 1
@@ -34,8 +42,14 @@ class MyRequestHandler(SocketServer.BaseRequestHandler):
                 if dic["food"] != 0:
                     foods = dic["food"]
             sendData["data"] = data
+            # print self.client_address,": \n",data,"\n"
             self.request.sendall(simplejson.dumps(sendData)+'\n')
+            lock.release()
            
 tcpServ = SocketServer.ThreadingTCPServer(ADDR, MyRequestHandler) 
 print 'waiting for connection...' 
-tcpServ.serve_forever()
+while True:
+    try:
+        tcpServ.serve_forever(poll_interval=0.5)
+    except:
+        continue
